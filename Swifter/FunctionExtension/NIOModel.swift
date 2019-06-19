b996b1ce53f41dbb085bab53854c5cb47f67e951
chunyang li <chunyang.li.o@nio.com>
2019-06-19 11:12:09 +0800

    新增 model工具

--- Swifter/FunctionExtension/NIOModel.swift
+++ Swifter/FunctionExtension/NIOModel.swift
@@ -1,23 +1,22 @@
 import Foundation
 import UIKit
 
-// MARK: 模型转Data
+// MARK: Model转Data
 public func toData<T>(by model: T) -> Data? where T: Encodable {
-    var data: Data?
     do {
-        data = try JSONEncoder().encode(model)
+        return try JSONEncoder().encode(model)
     } catch {
+        debugPrint(error)
         return nil
     }
-    return data
 }
 
-// MARK: 模型转JSON字符串
+// MARK: Model转JSON字符串
 public func toString<T>(by model: T) -> String? where T: Encodable {
     return toData(by: model)?.toString()
 }
 
-// MARK: 模型转JSON对象
+// MARK: Model转JSON对象
 public func toObject<T>(by model: T) -> Any? where T: Encodable {
     return toData(by: model)?.toObject()
 }
@@ -31,18 +30,48 @@ public extension Dictionary {
     // MARK: JSON对象转Data
     func toData() -> Data? {
         if !JSONSerialization.isValidJSONObject(self) { return nil }
-        var data: Data? = nil
         do {
-            data = try JSONSerialization.data(withJSONObject: self, options: JSONSerialization.WritingOptions.prettyPrinted)
+            return try JSONSerialization.data(withJSONObject: self, options: JSONSerialization.WritingOptions.prettyPrinted)
         } catch {
-            return data
+            debugPrint(error)
+            return nil
         }
-        return data
     }
     
     // MARK: JSON对象转JSON字符串
     func toString() -> String? {
         return self.toData()?.toString()
+    }
+}
+
+public extension Array where Element: Codable {
+    func toData() -> Data? {
+        do {
+            return try JSONEncoder().encode(self)
+        } catch {
+            debugPrint(error)
+            return nil
+        }
+    }
+    
+    func toString() -> String? {
+        guard let data: Data = self.toData() else {
+            return nil
+        }
+        return String(data: data, encoding: String.Encoding.utf8)
+    }
+    
+    func toObject() -> Any? {
+        guard let data: Data = self.toData() else {
+            return nil
+        }
+        if JSONSerialization.isValidJSONObject(data) { return nil }
+        do {
+            return try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+        } catch {
+            debugPrint(error)
+            return nil
+        }
     }
 }
 
@@ -55,56 +84,115 @@ public extension String {
     // MARK: JSON字符串转Model
     func toModel<T>(type: T.Type) -> T? where T: Decodable {
         guard let data: Data = self.data(using: String.Encoding.utf8) else { return nil }
-        var model: T?
         do {
-            model = try JSONDecoder().decode(type, from: data)
+            return try JSONDecoder().decode(type, from: data)
         } catch {
-            return model
+            debugPrint(error)
+            return nil
         }
-        return model
     }
     
     // MARK: JSON字符串转JSON对象
     func toObject() -> Any? {
         guard let data: Data = self.data(using: String.Encoding.utf8) else { return nil }
         if JSONSerialization.isValidJSONObject(data) { return nil }
-        var jsonObject: Any?
         do {
-            jsonObject = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+            return try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
         } catch {
-            return jsonObject
+            debugPrint(error)
+            return nil
         }
-        return jsonObject
     }
 }
 
 public extension Data {
     // MARK: JSON二进制转Model
     func toModel<T>(type: T.Type) -> T? where T: Decodable {
-        var model: T?
         do {
             /// 会自动h过滤Value为nil的项
-            model = try JSONDecoder().decode(type, from: self)
+            return try JSONDecoder().decode(type, from: self)
         } catch {
-            return model
+            debugPrint(error)
+            return nil
         }
-        return model
     }
     
     // MARK: JSON二进制转JSON对象
     func toObject() -> Any? {
-        var jsonObject: Any?
         do {
-            jsonObject = try JSONSerialization.jsonObject(with: self, options: JSONSerialization.ReadingOptions.mutableContainers)
+            return try JSONSerialization.jsonObject(with: self, options: JSONSerialization.ReadingOptions.mutableContainers)
         } catch {
-            return jsonObject
+            debugPrint(error)
+            return nil
         }
-        return jsonObject
     }
     
     // MARK: JSON二进制转JSON字符串
     func toString() -> String? {
         return String(data: self, encoding: String.Encoding.utf8)
+    }
+}
+
+public extension Encodable {
+    func toData() -> Data? {
+        do {
+            return try JSONEncoder().encode(self)
+        } catch {
+            debugPrint(error)
+            return nil
+        }
+    }
+    
+    func toString() -> String? {
+        guard let data: Data = self.toData() else {
+            return nil
+        }
+        return String(data: data, encoding: String.Encoding.utf8)
+    }
+    
+    func toObject() -> Any? {
+        guard let data = self.toData() else { return nil }
+        do {
+            return try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+        } catch {
+            return nil
+        }
+    }
+}
+
+public extension Decodable {
+    static func toModel(from data: Data?) -> Self? {
+        guard let `data`: Data = data else { return nil }
+        do {
+            return try JSONDecoder().decode(Self.self, from: data)
+        } catch {
+            debugPrint(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    static func toModel(from dictionary: [AnyHashable: Any]?) -> Self? {
+        guard let `dictionary`: [AnyHashable: Any] = dictionary, let jsonData = try? JSONSerialization.data(withJSONObject: dictionary) else {
+            return nil
+        }
+        do {
+            return try JSONDecoder().decode(Self.self, from: jsonData)
+        } catch {
+            debugPrint(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    static func toModel(from string: String?) -> Self? {
+        guard let `string`: String = string, let jsonData = string.data(using: .utf8) else {
+            return nil
+        }
+        do {
+            return try JSONDecoder().decode(Self.self, from: jsonData)
+        } catch {
+            debugPrint(error.localizedDescription)
+            return nil
+        }
     }
 }
 
