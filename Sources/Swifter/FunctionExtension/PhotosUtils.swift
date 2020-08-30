6795886f93526853d396948ab0ff8fc8c1e54ded
Yang <BetrayalPromise@gmail.com>
2020-08-30 18:01:17 +0800

    修正 iOS9及其以上适用

--- Sources/Swifter/FunctionExtension/PhotosUtils.swift
+++ Sources/Swifter/FunctionExtension/PhotosUtils.swift
@@ -3,12 +3,14 @@ import Photos
 
 public extension PHAsset {
     /// 容量 单位是字节
+    @available(iOS 9, *)
     var volume: Double? {
         let resources: [PHAssetResource] = PHAssetResource.assetResources(for: self)
         guard let size: Double = resources.first?.value(forKey: "fileSize") as? Double else { return nil }
         return size
     }
     
+    @available(iOS 9, *)
     var fileName: String? {
         let resources: PHAssetResource? = PHAssetResource.assetResources(for: self).first
         return resources?.originalFilename
